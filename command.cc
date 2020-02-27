0e4f0703fe94ecf1026c3e0c40a7981b4f569dc6
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-02-27 18:14:54 -0500

    0 Mon Feb 17 09:54:55 EST 2020

--- command.cc
+++ command.cc
@@ -107,7 +107,7 @@ void Command::execute() {
   // and call exec
 
  // Create a new process
-cout << _simpleCommandsArray[0] << endl;
+cout << _simpleCommandsArray[0].c_str << endl;
 /*int ret;
 for ( int i = 0;i < _simpleCommandsArray.size() ;i++ ) {
 ret = fork();
