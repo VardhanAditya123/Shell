0fc40f6e4331a0defd89dc93e064f39ebb0d6e77
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-02-27 18:28:31 -0500

    0 Mon Feb 17 09:54:55 EST 2020

--- simpleCommand.cc
+++ simpleCommand.cc
@@ -16,9 +16,6 @@ SimpleCommand::~SimpleCommand() {
   }
 }
 
-SimpleCommand:: firstCommand() {
-retutrn _argumentsArray[0];
-}
 
 void SimpleCommand::insertArgument( std::string * argument ) {
   // simply add the argument to the vector
