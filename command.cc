0379d8d64a967bbc94e719c98b2cccc24989709c
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-02-27 00:22:14 -0500

    0 Mon Feb 17 09:54:55 EST 2020

--- command.cc
+++ command.cc
@@ -40,9 +40,8 @@ void Command::insertSimpleCommand( SimpleCommand * simpleCommand ) {
 void Command::clear() {
     // deallocate all the simple commands in the command vector
     for (auto simpleCommand : _simpleCommandsArray) {
-        if(simpleCommand != null){
+        if(simpleCommand != NULL){
         delete simpleCommand;
-        }
     }
      // remove all references to the simple commands we've deallocated
     // (basically just sets the size to 0)
