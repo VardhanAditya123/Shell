9286c7d10053a9278841117c8698739fe9385d4e
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-02-27 18:07:55 -0500

    0 Mon Feb 17 09:54:55 EST 2020

--- command.cc
+++ command.cc
@@ -111,7 +111,7 @@ for ( int i = 0;i < _simpleCommandsArray.size() ;i++ ) {
 ret = fork();
 if (ret == 0) {
 //child
-execvp(&(_simpleCommandsArray[i])->_argumentsArray[0], _simpleCommandsArray[i]->_argumentsArray);
+execvp(_simpleCommandsArray[i]->_argumentsArray[0], _simpleCommandsArray[i]->_argumentsArray);
 perror("execvp");
 _exit(1);
 }
