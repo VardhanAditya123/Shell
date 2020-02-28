b7ff75265e3c8c2fa2dd43ab87af5e0f458cece9
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-02-28 01:10:11 -0500

    0 Mon Feb 17 09:54:55 EST 2020

--- command.cc
+++ command.cc
@@ -120,11 +120,10 @@ ret = fork();
 std::vector <string*> v ;
 
 for(unsigned j = 0 ; j < (_simpleCommandsArray[i]->_argumentsArray).size();j++){
-  cout<< (string*) (_simpleCommandsArray[i]->_argumentsArray[j]) <<endl;
   v.push_back((string*) &(_simpleCommandsArray[i]->_argumentsArray[j])); 
 }
 
-char* const* v1 = (char*const*)(v.data());
+char* const* v1 = change_type(string*v,char**final )
 
 if (ret == 0) {
 //child
