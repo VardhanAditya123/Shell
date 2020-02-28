720553a3842dbc6c49308f2a9123ec79740254ca
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-02-27 22:20:45 -0500

    0 Mon Feb 17 09:54:55 EST 2020

--- command.cc
+++ command.cc
@@ -115,6 +115,7 @@ int ret;
 for ( unsigned int i = 0; i < _simpleCommandsArray.size() ;i++ ) {
 ret = fork();
 
+vector<char* const* >arr ;
 
 for ( unsigned int j = 0; j <(_simpleCommandsArray[i]->_argumentsArray).size(); j++) 
  arr.push_back((char* const*) (_simpleCommandsArray[i]->_argumentsArray[j])); 
