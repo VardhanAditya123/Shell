bfdb8add89fa5586cd0518e35ac8701c8de62584
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-02-28 02:07:51 -0500

    0 Mon Feb 17 09:54:55 EST 2020

--- command.cc
+++ command.cc
@@ -99,7 +99,7 @@ void change_type( vector <string*> v , char** final){
 for(unsigned int i =0 ; i < v.size();i++){
   cout << v.at(i)->c_str() << endl;
 
-  final[i] = (const_cast<char*>((char*) v.at(i)->c_str()));
+  final[i] = ( const_cast<char*>((char*) v.at(i)->c_str()) );
 }
 }
 void Command::execute() {
