856ee3c76e0b4d8213ba3767f6ef9a8807ba131c
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-02-27 23:30:32 -0500

    0 Mon Feb 17 09:54:55 EST 2020

--- command.cc
+++ command.cc
@@ -121,8 +121,8 @@ for ( unsigned int j = 0; j <(_simpleCommandsArray[i]->_argumentsArray).size(); 
 //  cout << *arr[j] << endl;
  }
  
-char*const* v = (&arr[0]) ;
-//  cout << *v[0] <<"lol"<< endl;
+char*const* v = (char*const*)(&arr[0]) ;
+ cout << *v[0] <<"lol"<< endl;
 
 
 if (ret == 0) {
