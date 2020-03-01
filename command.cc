153bd57b0920fa68b93bcec73fe91c35b0d99868
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-03-01 14:16:24 -0500

    0 Mon Feb 17 09:54:55 EST 2020 0 Sun Mar 1 12:50:37 EST 2020 0 Sun Mar 1 12:55:51 EST 2020 12 Sun Mar 1 13:06:07 EST 2020 17 Sun Mar 1 13:49:13 EST 2020 12 Sun Mar 1 13:49:47 EST 2020 12 Sun Mar 1 14:00:21 EST 2020 17 Sun Mar 1 14:01:54 EST 2020 16 Sun Mar 1 14:12:01 EST 2020 16 Sun Mar 1 14:12:34 EST 2020 16 Sun Mar 1 14:13:10 EST 2020 16 Sun Mar 1 14:14:00 EST 2020

--- command.cc
+++ command.cc
@@ -119,7 +119,7 @@ int tmpout=dup(1);
 //set the initial input
 int fdin;
 if (_inFileName) {
-fdin = open(_inFileName->c_str(), O_RDONLY,0400);
+fdin = open(_inFileName->c_str(), O_RDONLY,0666);
 }
 else {
 // Use default input
@@ -141,9 +141,9 @@ if (count == _simpleCommandsArray.size()-1){
 // Last simple command
 if(_outFileName){
 if(_append)
-fdout=open(_outFileName->c_str() ,O_APPEND | O_CREAT | O_RDONLY,0600);
+fdout=open(_outFileName->c_str() ,O_APPEND | O_CREAT | O_RDONLY,0666);
 else
-fdout=open(_outFileName->c_str() ,O_WRONLY | O_CREAT | O_TRUNC,0600);
+fdout=open(_outFileName->c_str() ,O_WRONLY | O_CREAT | O_TRUNC,0666);
 }
 else {
 // Use default output
