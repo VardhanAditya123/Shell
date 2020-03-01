e215160827d8a6fd174195061451291404150285
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-03-01 11:42:11 -0500

    0 Mon Feb 17 09:54:55 EST 2020

--- command.cc
+++ command.cc
@@ -169,6 +169,7 @@ char **final  = new char*[100];
 int c =0;
 for(auto & word : simpleCommand->_argumentsArray){
   final[c]=const_cast<char*>((word->c_str()));
+  cout << final[c] << endl;
   c=c+1;
 }
 
