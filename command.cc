b1133dfd86b8f0436740bbccdfd4cfeca86c91c8
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-03-05 18:53:23 -0500

    0 Mon Feb 17 09:54:55 EST 2020 0 Sun Mar 1 12:50:37 EST 2020 0 Sun Mar 1 12:55:51 EST 2020 12 Sun Mar 1 13:06:07 EST 2020 17 Sun Mar 1 13:49:13 EST 2020 12 Sun Mar 1 13:49:47 EST 2020 12 Sun Mar 1 14:00:21 EST 2020 17 Sun Mar 1 14:01:54 EST 2020 16 Sun Mar 1 14:12:01 EST 2020 16 Sun Mar 1 14:12:34 EST 2020 16 Sun Mar 1 14:13:10 EST 2020 16 Sun Mar 1 14:14:00 EST 2020 16 Sun Mar 1 14:16:39 EST 2020 16 Sun Mar 1 14:17:09 EST 2020 16 Sun Mar 1 14:20:01 EST 2020 16 Sun Mar 1 14:23:48 EST 2020 16 Sun Mar 1 16:03:39 EST 2020 16 Sun Mar 1 16:08:55 EST 2020 16 Sun Mar 1 16:11:16 EST 2020 16 Sun Mar 1 16:21:46 EST 2020 16 Sun Mar 1 16:23:19 EST 2020 17 Sun Mar 1 16:24:07 EST 2020 17 Sun Mar 1 16:34:56 EST 2020 17 Sun Mar 1 16:35:22 EST 2020 17 Sun Mar 1 16:38:46 EST 2020 18 Sun Mar 1 17:19:42 EST 2020 18 Sun Mar 1 17:30:06 EST 2020 18 Sun Mar 1 17:33:14 EST 2020 18 Sun Mar 1 17:39:56 EST 2020 18 Sun Mar 1 18:10:31 EST 2020 18 Sun Mar 1 18:21:35 EST 2020 18 Sun Mar 1 18:22:39 EST 2020 18 Sun Mar 1 18:27:59 EST 2020 19 Sun Mar 1 18:50:58 EST 2020 19 Sun Mar 1 19:29:33 EST 2020 19 Sun Mar 1 19:37:28 EST 2020 19 Sun Mar 1 19:40:42 EST 2020 19 Sun Mar 1 19:55:13 EST 2020 19 Sun Mar 1 19:59:46 EST 2020 20 Sun Mar 1 20:08:56 EST 2020 20 Sun Mar 1 20:29:04 EST 2020 1 Sun Mar 1 20:30:31 EST 2020 20 Sun Mar 1 20:34:00 EST 2020 20 Sun Mar 1 20:35:34 EST 2020 20 Thu Mar 5 10:39:28 EST 2020 20 Thu Mar 5 10:40:20 EST 2020 20 Thu Mar 5 10:57:52 EST 2020 20 Thu Mar 5 11:11:29 EST 2020 20 Thu Mar 5 11:14:26 EST 2020 21 Thu Mar 5 13:05:48 EST 2020 21 Thu Mar 5 13:46:41 EST 2020 21 Thu Mar 5 14:21:23 EST 2020 21 Thu Mar 5 14:25:15 EST 2020 21 Thu Mar 5 14:25:43 EST 2020 21 Thu Mar 5 14:32:04 EST 2020 21 Thu Mar 5 14:42:48 EST 2020 22 Thu Mar 5 15:09:17 EST 2020 22 Thu Mar 5 15:30:38 EST 2020 22 Thu Mar 5 15:39:25 EST 2020 22 Thu Mar 5 16:51:59 EST 2020 22 Thu Mar 5 17:14:17 EST 2020 6 Thu Mar 5 17:47:40 EST 2020 24 Thu Mar 5 18:05:00 EST 2020 24 Thu Mar 5 18:06:58 EST 2020

--- command.cc
+++ command.cc
@@ -135,7 +135,7 @@ if(s1.compare(s2) == 0){
   // print();
 
 if(s1.compare("printenv")==0){
-  char n[] = "NULL";
+  char *n = (char*)malloc(strlen("NULL")+1);
   for(int i = 0 ;; i++){
     if(strcmp(environ[i],n )==0){
      Shell::prompt();
