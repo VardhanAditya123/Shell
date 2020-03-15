65dcaafb81eda6f1dfda424a8e5fbb601c964563
Aditya Vardhan Merla <amerla@data.cs.purdue.edu>
2020-03-14 20:23:51 -0400

    0 Mon Feb 17 09:54:55 EST 2020 0 Sun Mar 1 12:50:37 EST 2020 0 Sun Mar 1 12:55:51 EST 2020 12 Sun Mar 1 13:06:07 EST 2020 17 Sun Mar 1 13:49:13 EST 2020 12 Sun Mar 1 13:49:47 EST 2020 12 Sun Mar 1 14:00:21 EST 2020 17 Sun Mar 1 14:01:54 EST 2020 16 Sun Mar 1 14:12:01 EST 2020 16 Sun Mar 1 14:12:34 EST 2020 16 Sun Mar 1 14:13:10 EST 2020 16 Sun Mar 1 14:14:00 EST 2020 16 Sun Mar 1 14:16:39 EST 2020 16 Sun Mar 1 14:17:09 EST 2020 16 Sun Mar 1 14:20:01 EST 2020 16 Sun Mar 1 14:23:48 EST 2020 16 Sun Mar 1 16:03:39 EST 2020 16 Sun Mar 1 16:08:55 EST 2020 16 Sun Mar 1 16:11:16 EST 2020 16 Sun Mar 1 16:21:46 EST 2020 16 Sun Mar 1 16:23:19 EST 2020 17 Sun Mar 1 16:24:07 EST 2020 17 Sun Mar 1 16:34:56 EST 2020 17 Sun Mar 1 16:35:22 EST 2020 17 Sun Mar 1 16:38:46 EST 2020 18 Sun Mar 1 17:19:42 EST 2020 18 Sun Mar 1 17:30:06 EST 2020 18 Sun Mar 1 17:33:14 EST 2020 18 Sun Mar 1 17:39:56 EST 2020 18 Sun Mar 1 18:10:31 EST 2020 18 Sun Mar 1 18:21:35 EST 2020 18 Sun Mar 1 18:22:39 EST 2020 18 Sun Mar 1 18:27:59 EST 2020 19 Sun Mar 1 18:50:58 EST 2020 19 Sun Mar 1 19:29:33 EST 2020 19 Sun Mar 1 19:37:28 EST 2020 19 Sun Mar 1 19:40:42 EST 2020 19 Sun Mar 1 19:55:13 EST 2020 19 Sun Mar 1 19:59:46 EST 2020 20 Sun Mar 1 20:08:56 EST 2020 20 Sun Mar 1 20:29:04 EST 2020 1 Sun Mar 1 20:30:31 EST 2020 20 Sun Mar 1 20:34:00 EST 2020 20 Sun Mar 1 20:35:34 EST 2020 20 Thu Mar 5 10:39:28 EST 2020 20 Thu Mar 5 10:40:20 EST 2020 20 Thu Mar 5 10:57:52 EST 2020 20 Thu Mar 5 11:11:29 EST 2020 20 Thu Mar 5 11:14:26 EST 2020 21 Thu Mar 5 13:05:48 EST 2020 21 Thu Mar 5 13:46:41 EST 2020 21 Thu Mar 5 14:21:23 EST 2020 21 Thu Mar 5 14:25:15 EST 2020 21 Thu Mar 5 14:25:43 EST 2020 21 Thu Mar 5 14:32:04 EST 2020 21 Thu Mar 5 14:42:48 EST 2020 22 Thu Mar 5 15:09:17 EST 2020 22 Thu Mar 5 15:30:38 EST 2020 22 Thu Mar 5 15:39:25 EST 2020 22 Thu Mar 5 16:51:59 EST 2020 22 Thu Mar 5 17:14:17 EST 2020 6 Thu Mar 5 17:47:40 EST 2020 24 Thu Mar 5 18:05:00 EST 2020 24 Thu Mar 5 18:06:58 EST 2020 24 Thu Mar 5 18:56:48 EST 2020 24 Thu Mar 5 19:15:41 EST 2020 25 Thu Mar 5 19:51:22 EST 2020 25 Thu Mar 5 20:12:44 EST 2020 25 Thu Mar 5 20:17:55 EST 2020 25.5 Thu Mar 5 20:21:05 EST 2020 26.0 Thu Mar 5 20:23:16 EST 2020 26.5 Thu Mar 5 20:28:34 EST 2020 27.0 Thu Mar 5 23:17:26 EST 2020 29.5 Fri Mar 13 13:16:18 EDT 2020 37.0 Fri Mar 13 14:14:25 EDT 2020 37.5 Sat Mar 14 16:09:17 EDT 2020 37.5 Sat Mar 14 16:18:22 EDT 2020 37.5 Sat Mar 14 16:19:16 EDT 2020 29.5 Sat Mar 14 16:21:24 EDT 2020 37.5 Sat Mar 14 16:26:33 EDT 2020 39.5 Sat Mar 14 16:52:55 EDT 2020 39.5 Sat Mar 14 17:24:40 EDT 2020 39.5 Sat Mar 14 17:33:40 EDT 2020 39.5 Sat Mar 14 17:34:19 EDT 2020

--- command.cc
+++ command.cc
@@ -356,6 +356,7 @@ void Command::execute() {
     int c =0;
     for(auto & word : simpleCommand->_argumentsArray){
        if(word->at(0)=='$'){
+         word=const_cast<char*>(word->c_str());
          word=getenv(word);
        }
        char*str = esc((char*)word->c_str());
